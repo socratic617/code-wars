6f786e876811c4b941c891db8696873678133b25
socratic617 <erik.solis02135@gmail.com>
2023-10-03 19:06:33 -0400

    Week 6 Code War Solutions

---
+++ convertBooleanToStrings.js
@@ -0,0 +1,6 @@
+function booleanToString(b) {
+  //your code here
+  //return either true or false because there are two options Boolean
+  //so you can use a ternary operator
+  return b ? "true" : "false";
+}
---
+++ convertBooleanValuesToStringYesorNo.js
@@ -0,0 +1,4 @@
+function boolToWord(bool) {
+  //I can use a ternary operator due to the options yes or no
+  return bool ? "Yes" : "No";
+}
