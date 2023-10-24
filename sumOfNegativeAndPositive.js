ea3c5bf0f49b384021297a86518658cd95b34346
socratic617 <erik.solis02135@gmail.com>
2023-10-24 00:12:07 -0400

    update line 8

--- sumOfNegativeAndPositive.js
+++ sumOfNegativeAndPositive.js
@@ -1,12 +1,11 @@
 function countPositivesSumNegatives(input) {
-  if (input === null) return []
-  if ( input.filter( n=> n != 0).length == 0) return [];
-  let countOfPositives =0; 
-  let sumOfNegatives =0;
+  if (input === null) return [];
+  if (input.filter((n) => n != 0).length == 0) return [];
+  let countOfPositives = 0;
+  let sumOfNegatives = 0;
   for (const n of input) {
-    if ( n < 0) sumOfNegatives += n;
-    if ( n > 0) sumOfPositives += 1;
-    
+    if (n < 0) sumOfNegatives += n;
+    if (n > 0) countOfPositives += 1;
   }
-  return [countOfPositives, sumOfNegatives]
+  return [countOfPositives, sumOfNegatives];
 }
