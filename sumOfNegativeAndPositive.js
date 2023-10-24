2e630b7b27114c7c17c471860dcad0230dd76b54
socratic617 <erik.solis02135@gmail.com>
2023-10-24 00:00:17 -0400

    Week9 Solutions Code Wars

---
+++ sumOfNegativeAndPositive.js
@@ -0,0 +1,12 @@
+function countPositivesSumNegatives(input) {
+  if (input === null) return []
+  if ( input.filter( n=> n != 0).length == 0) return [];
+  let countOfPositives =0; 
+  let sumOfNegatives =0;
+  for (const n of input) {
+    if ( n < 0) sumOfNegatives += n;
+    if ( n > 0) sumOfPositives += 1;
+    
+  }
+  return [countOfPositives, sumOfNegatives]
+}
