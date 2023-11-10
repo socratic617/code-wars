21b47e831888dbcaad3804bbe11be8fcb0a5637c
socratic617 <erik.solis02135@gmail.com>
2023-11-10 11:50:36 -0500

    Week11 Solutions  Code War

---
+++ reverseArr.js
@@ -0,0 +1,14 @@
+function fixTheMeerkat (arr) {
+  //use reverse method to reverse arrays order
+  return arr.reverse()
+}
+
+
+
+// function fixTheMeerkat(arr) {
+//   let resultArr = []; // create new array  to store result 
+//   for(let i = arr.length-1; i >= 0; i--){ //create for loop and have it start at the very end "arr.length -1" , have it decrement down going backwards through array
+//    resultArr.push(arr[i]) // use method .push to add it to resultArr 
+//   }
+//  return resultArr; // return the final result in resultArr
+// }
