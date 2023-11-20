d5987bf720ff9ead4a0ad1abc0e449ba20162ff7
socratic617 <erik.solis02135@gmail.com>
2023-11-20 11:31:23 -0500

    Week13 Solutions Code Wars

---
+++ returnStringWName.js
@@ -0,0 +1,11 @@
+function greet(name){
+  //use template literal 
+  return `Hello, ${name} how are you doing today?`
+}
+
+// or concat method
+
+function greet(name){
+  //use concatenation
+  return 'Hello' + name + 'how are you doing today?'
+}
