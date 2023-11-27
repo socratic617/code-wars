98503c898e5cf5c358c32db8128c4f2ea0d20bd3
socratic617 <erik.solis02135@gmail.com>
2023-11-27 12:26:44 -0500

    Week14 Solutions Code War

---
+++ schoolPaperWork.js
@@ -0,0 +1,8 @@
+function paperwork(n, m) {
+  if( n < 0 || m < 0 ){// by default whether n or m is less than zero it still equals 0
+    return 0
+  }
+  else {
+    return n * m // if (n) pages  is one schoolwork assignment and i have m number of classmates multiply n number of pagesto see how many pages i need in total to get product (product is when you multiply)
+  }
+}
