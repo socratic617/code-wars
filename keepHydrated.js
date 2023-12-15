145f01102647d1f0820bd29a5c05e2a9b328cbee
socratic617 <erik.solis02135@gmail.com>
2023-12-14 23:07:44 -0500

    Week16 Solutions Code Wars

---
+++ keepHydrated.js
@@ -0,0 +1,12 @@
+function litres(time) {
+  //.5 liters of water per hour
+  //create liter variable
+  //create result variable for calculation
+  // use method math.floor to round to nearest number
+  // do math equations multiplication
+  let litres = .5
+  let result = Math.floor(litres * time)
+  console.log("Result : ", result)
+  return result
+
+}
