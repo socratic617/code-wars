21e4969049946508eb05b399ca3151701cf81ed8
socratic617 <erik.solis02135@gmail.com>
2024-01-24 23:40:31 -0500

    update

--- deleteFirstAndLastCharacterOfString.js
+++ deleteFirstAndLastCharacterOfString.js
@@ -2,7 +2,7 @@ function removeChar(str) {
   //You got this!
   //use condtional to check if string has at the very least two characters
   if (str.length >= 2) {
-    // if there is two characters, use method substring to remove first character (at index 0) and last character using str.length using "-1"
+
     return str.substring(1, str.length - 1);
   } else {
     //if there is no characters at the very least two return an empty string
