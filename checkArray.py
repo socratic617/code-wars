bd76a88f7a396155ede6b452a5396a0b777d5a56
socratic617 <erik.solis02135@gmail.com>
2024-04-30 16:42:35 -0400

    Psuedo code solution

--- checkArray.py
+++ checkArray.py
@@ -4,3 +4,11 @@ For every good kata idea there seem to be quite a few bad ones!
 
 In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 '''
+#Solution: 
+""" 
+Goal: Check array "x" 
+ Use two conditionals 
+ if x has more then 1 or just 2 good idea return "Publish"
+ else if x has more then 2 good idea return "I smell a series!"
+ else return "Fail!"
+"""
