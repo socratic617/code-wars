b2007cb4a8c10a5ad79cf34fd43ad60403498d3e
socratic617 <erik.solis02135@gmail.com>
2024-08-09 15:35:33 -0400

    Psuedo code

---
+++ willThereBeEnoughSpace.py
@@ -0,0 +1,15 @@
+def enough(cap, on, wait):
+    # Your code here
+    '''
+    @param: cap - amount of people the bus can hold excluding the driver.
+    @param on - the number of people on the bus excluding the driver.
+    @param wait -  the number of people waiting to get on to the bus excluding the driver.
+    @return : datatype - num
+    Use conditionals
+        if there is enough space return 0
+        if there is not enough space return "the number of passagers the driver cant take"
+    ''' 
+    print("this is the cap: ", cap)
+    print("this is on :", on)
+    print("this is the wait :", wait )
+   
