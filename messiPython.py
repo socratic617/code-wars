9bc5c438ce8ac7d02d9952ab7c73eac54cb1bcad
socratic617 <erik.solis02135@gmail.com>
2024-08-08 23:11:51 -0400

    Solution 1 Messi goals

---
+++ messiPython.py
@@ -0,0 +1,8 @@
+def goals(laLiga, copaDelRey, championsLeague):
+    total_goals = laLiga+copaDelRey+championsLeague
+    print("This is La liga goals : ", laLiga)
+    print("This is copaDelRey goals : ", copaDelRey) 
+    print("This is championsLeague goals : ", championsLeague)
+    print("This is the total goals : ", total_goals) 
+    
+    return total_goals
